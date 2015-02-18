9a30140f57341627e83c12b60cda79c6a728c80b
Emil Olsson <emilolsson93@gmail.com>
2015-02-18 22:31:33 +0100

    simple connection with chromecast from app without error handling and teardown

--- app/src/main/java/com/emilochhektor/quizcous/MainActivity.java
+++ app/src/main/java/com/emilochhektor/quizcous/MainActivity.java
@@ -1,16 +1,166 @@
 package com.emilochhektor.quizcous;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+//import android.support.v7.media.MediaRouteSelector;
+//import android.support.v7.media.MediaRouter;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private static String TAG = "com.emilochhektor.quizcous.MainActivity";
+    private static String APP_ID = "B7BD0161";
+
+
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MyMediaRouterCallback mMediaRouterCallback;
+
+    private CastDevice mCastDevice;
+    private Cast.Listener mCastListener;
+    private GoogleApiClient mApiClient;
+
+    private ConnectionCallbacks mConnectionCallbacks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initMediaRouter();
+    }
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
+    protected void onStop() {
+        if (isFinishing()) {
+            mMediaRouter.removeCallback(mMediaRouterCallback);
+        }
+        super.onPause();
+    }
+
+    private void initMediaRouter() {
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast(APP_ID))
+                .build();
+
+        mMediaRouterCallback = new MyMediaRouterCallback();
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider)MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+        return true;
+    }
+
+
+
+
+    private void launchReceiver() {
+        try {
+            mCastListener = new Cast.Listener() {
+
+            };
+
+
+            mConnectionCallbacks = new ConnectionCallbacks();
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions.builder(mCastDevice, mCastListener);
+
+            mApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .build();
+
+            mApiClient.connect();
+
+        } catch (Exception e) {
+            Log.d(TAG, "Lol exception: " + e.toString());
+        }
+    }
+
+
+
+
+    public class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mCastDevice = CastDevice.getFromBundle(info.getExtras());
+            launchReceiver();
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mCastDevice = null;
+        }
+    }
+
+
+
+    private class ConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks {
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            try {
+                Cast.CastApi.launchApplication(mApiClient, APP_ID, false)
+                    .setResultCallback(
+                        new ResultCallback<Cast.ApplicationConnectionResult>() {
+                            @Override
+                            public void onResult(Cast.ApplicationConnectionResult applicationConnectionResult) {
+                                Status status = applicationConnectionResult.getStatus();
+                                if (status.isSuccess()) {
+
+                                } else {
+                                    Log.d(TAG, "No success on status, lol");
+                                }
+                            }
+                        }
+                    );
+            } catch (Exception e) {
+                Log.d(TAG, "Failed to launch application", e);
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            Log.d(TAG, "Connection suspended");
+        }
     }
 }
---
+++ app/src/main/java/com/emilochhektor/quizcous/MyMediaRouterCallback.java
@@ -0,0 +1,7 @@
+package com.emilochhektor.quizcous;
+
+import android.support.v7.media.MediaRouter;
+
+import com.google.android.gms.cast.CastDevice;
+
+
